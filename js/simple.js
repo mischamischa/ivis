79f577843cca63d4ff420ec0214e6e5d6c24c564
Mischa Nüesch <mischa.nueesch@students.fhnw.ch>
2016-05-03 13:02:57 +0200

    server fetch

--- js/simple.js
+++ js/simple.js
@@ -1,13 +1,45 @@
 var cy = cytoscape({
     container: document.getElementById('cy'),
 
-    elements: {
-        nodes: [
-            { data: { id: '0'} },
-            { data: { id: '1'} },
-        ],
-        edges: [
-            { data: { source: '0', target: '1' } },
-        ]
-    },
+    style: [
+        {
+            selector: 'node',
+            style: {
+                'background-color': '#666',
+                'label': 'data(name)'
+            }
+        },
+        {
+            selector: 'edge',
+            style: {
+                'width': 3,
+                'target-arrow-color': '#ccc',
+                'target-arrow-shape': 'triangle'
+            }
+        }
+    ],
 });
+
+
+var req = new XMLHttpRequest();
+req.addEventListener("load", reqListener);
+req.open("GET", "http://server1102.cs.technik.fhnw.ch/json.php?t=Ort&n=200&c=GEONAME,SPRACODE");
+req.send();
+
+function reqListener () {
+    var langs = new Set();
+    rows = JSON.parse(this.responseText);
+    cy.startBatch();
+    for(r in rows) {
+        obj = rows[r];
+        cy.add({group: "nodes", data: {id:r, name:obj['GEONAME']}});
+        lang = obj['SPRACODE'];
+        if(!langs.has(lang)) {
+            langs.add(lang)
+            cy.add({group: "nodes", data: {id:lang, name:lang}});
+        }
+        cy.add({group: "edges", data: {source:r, target:lang}});
+    }
+    cy.endBatch();
+    cy.elements().layout({ name: 'cose' });
+}
